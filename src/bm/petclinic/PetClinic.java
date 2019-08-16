5f17128499b06fc8b56b6cd77809232869cd7ba4
bm-sushma <39586586+bm-sushma@users.noreply.github.com>
2019-08-16 15:45:28 +0530

    Update PetClinic.java

--- src/bm/petclinic/PetClinic.java
+++ src/bm/petclinic/PetClinic.java
@@ -34,7 +34,8 @@ public class PetClinic {
 		//driver.get(url);
 		
 		//driver.get(url);
-		driver=new FirefoxDriver();
+		//driver=new FirefoxDriver();
+		System.setProperty("webdriver.firefox.bin","/usr/bin/firefox"); 
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(500, TimeUnit.SECONDS);
 		driver.get("http://54.69.137.122:8090/petclinic/");
